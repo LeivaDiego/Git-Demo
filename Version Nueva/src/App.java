81c3738140cfccfb72961e8c528f55de2bb8e248
LeivaDiego <lei21752@uvg.edu.gt>
2023-01-16 23:09:49 -0600

    Version 3

--- Version Nueva/src/App.java
+++ Version Nueva/src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Nuevo");
+
+        System.out.println("Version 3");
     }
 }
